5e872e448e246e07f218e3b1944409304eb7ac1e
Harold Duvan <78284715+HaroldDuvanPinillaSalinas@users.noreply.github.com>
2021-08-13 02:00:49 -0500

    Add files via upload

---
+++ Talleres/Taller 1/biseccion_punto4.R
@@ -0,0 +1,78 @@
+# BISECCION - PUNTO 4
+#f(c) = ((gm)/c)*(1-e^-(c/m)*t)-v
+f <- function(x) { (667.38/x)*(1-exp( -(x/68.1)*10 ))-40 }
+
+cant_decimales <- function(x, k) trimws(format(round(x, k), nsmall=k))
+raices <- list()
+
+falsaPosicion <- function(a,b) {
+  tol <- 10^-8
+  it <- 0
+  num_raiz <- 1
+  
+  s <- seq(a, b, 0.01)
+  plot(s, f(s), type = "l", col = "blue")
+  abline(h = 0, col = "red")
+  
+  if(f(a) != 0 && f(b) != 0) {
+    x_0 <- a
+    x_1 <- b
+    x_2 <- x_1 - ( (f(x_1)*(x_1-x_0)) / (f(x_1)-f(x_0)) )
+    error <- (x_1-x_0)/2
+    
+    if(f(x_0)*f(x_1) < 0) { #Tienen signos contrarios
+      
+      while(abs(f(x_2)) > tol) {
+        
+        if(f(x_0)*f(x_2) < 0) { #Tienen signos contrarios
+          x_0 <- x_0
+          x_1 <- x_2
+          x_2 <- (x_0+x_1)/2
+          
+          if(abs(f(x_2)) <= tol) {
+            raiz <- cant_decimales(x_2, 16)
+            raices[[num_raiz]] <- raiz
+            num_raiz <- num_raiz+1
+          }
+        }
+        else {
+          x_0 <- x_2  
+          x_1 <- x_1     
+          x_2 <- (x_0+x_1)/2
+          
+          if(abs(f(x_2)) <= tol) {
+            raiz <- cant_decimales(x_2, 16)
+            raices[[num_raiz]] <- raiz
+            num_raiz <- num_raiz+1
+          }
+        }
+        
+        error <- (x_1-x_0)/2
+        cat("\nIt: ", it+1, "\tX: ", cant_decimales(x_2, 8), "\tError: ", cant_decimales(error, 8))
+        
+        it <- it+1
+      }
+      
+    }
+    else{ 
+      cat("\nCon el método de la falsa posición no fue posible hallar raices en la función dada en el intervalo (", a, " , ", b, ")")
+    }
+  }
+  else if (f(a) == 0) {
+    raices[[num_raiz]] <- a
+  }
+  else {
+    raices[[num_raiz]] <- b
+  }
+  
+  if(length(raices) > 0) {
+    cat("\n\nLa raiz hayada mediante el método de la falsa posición fue: \n")
+    print(raices)
+  }
+  
+  for (i in raices) {
+    points(i,0)
+  }
+}
+
+falsaPosicion(14,16)
---
+++ Talleres/Taller 1/biseccion_punto5.R
@@ -0,0 +1,84 @@
+# BISECCION - PUNTO 5
+f <- function(t) { 3*sin(t)^3-1 }                                     
+g <- function(t) { 4*sin(t)*cos(t) }
+
+cant_decimales <- function(x, k) trimws(format(round(x, k), nsmall=k))
+raices <- list()
+
+biseccion <- function(a,b) {
+  tol <- 10^-8
+  it <- 0
+  num_raiz <- 1
+  
+  #GRAFICAS PUNTO 5
+  x=seq(0,2*pi,length=100)
+  plot(x,f(x),type="l",col="blue",lwd=3,main="f(t) y g(t)",xlab="",ylab="",las=1,col.axis="red")
+  lines(x,g(x),col="green",lwd=3)
+  legend("bottomleft",col=c("blue","green"),legend =c("f(t) = 3sin^3(t)-1","g(t) = 4sin(t)cos(t)"), lwd=3, bty = "n")
+  abline(h = 0, col = "black")
+  
+  if(f(a) != g(a) && f(b) != g(b)) {
+    x_0 <- a
+    x_1 <- b
+    x_2 <- (x_0+x_1)/2
+    error <- (x_1-x_0)/2
+    
+    if( (f(x_0) < g(x_0) && f(x_1) > g(x_1)) | (g(x_0) < f(x_0) && g(x_1) > f(x_1)) ) { #Se cruzan
+      
+      while(abs(f(x_2)-g(x_2)) > tol) {
+        
+        if( (f(x_0) < g(x_0) && f(x_2) > g(x_2)) | (g(x_0) < f(x_0) && g(x_2) > f(x_2)) ) { #Se cruzan
+          x_0 <- x_0
+          x_1 <- x_2
+          x_2 <- (x_0+x_1)/2
+          
+          if(abs(f(x_2)-g(x_2)) <= tol) {
+            raiz <- cant_decimales(x_2, 16)
+            raices[[num_raiz]] <- raiz
+            #raices[[num_raiz]] <- x_2
+            num_raiz <- num_raiz+1
+          }
+        }
+        else {
+          x_0 <- x_2  
+          x_1 <- x_1     
+          x_2 <- (x_0+x_1)/2 
+          
+          if(abs(f(x_2)-g(x_2)) <= tol) {
+            raiz <- cant_decimales(x_2, 16)
+            raices[[num_raiz]] <- raiz
+            #raices[[num_raiz]] <- x_2
+            num_raiz <- num_raiz+1
+          }
+        }
+        
+        error <- (x_1-x_0)/2
+        cat("\nIt: ", it+1, "\tX: ", cant_decimales(x_2, 8), "\tError: ", cant_decimales(error, 8))
+        
+        it <- it+1
+      }
+      
+    }
+    else{ 
+      cat("\nNo se pudo resolver en el intervalo (", a, " , ", b, "), intente con un intervalo diferente")
+    }
+  }
+  else if (f(a) == g(a)) {
+    raices[[num_raiz]] <- a
+  }
+  else {
+    raices[[num_raiz]] <- b
+  }
+  
+  if(length(raices) > 0) {
+    cat("\n\nLa raiz hayada mediante el método de la falsa posición fue: \n")
+    print(raices)
+  }
+  
+  for (i in raices) {
+    points(i,0)
+    abline(v = i, col = "red")
+  }
+}
+
+biseccion(0,2)
