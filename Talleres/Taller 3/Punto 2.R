90157786a0afa7a8bebc9058f1166144661fa3fa
Harold Duvan <78284715+HaroldDuvanPinillaSalinas@users.noreply.github.com>
2021-10-20 03:29:59 -0500

    Add files via upload

---
+++ Talleres/Taller 3/Punto 2.R
@@ -0,0 +1,17 @@
+#install.packages("pracma")
+
+library(pracma)
+#puntos dados (0, 10) (1, 15) (2, 5)
+x <- c(0,1,2)
+y <- c(10,15,5)
+
+#Secuencia 
+xi<- seq(0, 2, by=1)
+
+#Se genera la funcion 
+pp <- cubicspline(x,y,xi=NULL,endp2nd = FALSE, der = c(1,1))
+#ppfun <- funtion(xi) ppval(pp,xi)
+print(pp)
+
+#Grafica
+plot(x,y,type = 'b')
